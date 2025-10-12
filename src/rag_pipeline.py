51a0814a07d3ebfb265ba444498719a42ade237a
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-12 18:58:00 +0530

    chore: remove deprecated files
    - Remove scripts/rewrite_msg.sh (replaced by commit-msg hook)
    - Remove src/rag_pipeline.py (deprecated wrapper)
    - Remove src/sci_fi_dataset_collector.py (deprecated wrapper)

--- src/rag_pipeline.py
+++
@@ -1,5 +0,0 @@
-# This file is deprecated - use src/main.py instead
-from src.main import main
-
-if __name__ == "__main__":
-    main()
--- src/sci_fi_dataset_collector.py
+++
@@ -1,5 +0,0 @@
-# This file is deprecated - use src/data_fetcher.py instead
-from src.data_fetcher import main
-
-if __name__ == "__main__":
-    main()
